4f14c6dc05efdf7e035ade4cb2e185217784e6a8
Duke Nukem <joncherwin@gmail.com>
2023-03-03 18:52:44 -0600

    All routes completed for product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -71,6 +71,7 @@ router.post('/', (req, res) => {
 
 // update product
 router.put('/:id', (req, res) => {
+  
   // update product data
   Product.update(req.body, {
     where: {
@@ -111,8 +112,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
