35361be99dac8121857d18b7f607b1eebd8ad936
Duke Nukem <joncherwin@gmail.com>
2023-03-03 19:22:37 -0600

    Tag and Category routes filled out

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -70,8 +70,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
-  
+router.put('/:id', (req, res) => {  
   // update product data
   Product.update(req.body, {
     where: {
